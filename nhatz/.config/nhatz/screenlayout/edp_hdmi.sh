008b8ed0822918feeedf68b0b2d1ea05a914ced7
Nhatz HK <nhatz.info@gmail.com>
2017-10-05 21:02:50 -0400

    Added automatic screensaver toggle on multimonitor mode toggle

--- nhatz/.config/nhatz/screenlayout/edp_hdmi.sh
+++ nhatz/.config/nhatz/screenlayout/edp_hdmi.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1366x768 --pos 1360x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1360x768 --pos 0x0 --rotate normal --output DP2 --off
+
+xset -dpms s 0 0
